c960fa9b2bb09639fbf96edc918c18ed11e01f13
Stanislav Hadzhipopov <s.hadzhipopov@yahoo.com>
2022-12-04 12:18:19 +0100

    Add project files.

---
+++ RadzenControlsTest/Shared/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadzenControlsTest.Shared
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public int? ManagerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime HireDate { get; set; }
+        public string City { get; set; }
+    }
+}
